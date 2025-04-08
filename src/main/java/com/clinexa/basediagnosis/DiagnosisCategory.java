ca5a194c23304e6c922457c2e01f03aff9b94e1d
Nikita Serba <nikitaserba@icloud.com>
2025-04-08 19:05:52 +0300

    Update diagnosis category

--- src/main/java/com/clinexa/basediagnosis/DiagnosisCategory.java
+++ src/main/java/com/clinexa/basediagnosis/DiagnosisCategory.java
@@ -1,39 +1,35 @@
+/*
+ * This file is part of Clinexa DiagnosisBase.
+ *
+ * Clinexa DiagnosisBase is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Clinexa DiagnosisBase is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU Lesser General Public License and GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License and GNU General Public
+ * License along with Clinexa DiagnosisBase. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.clinexa.basediagnosis;
 
 public class DiagnosisCategory {
 
-    public enum Type {
-        DIAGNOSIS,
-        SYMPTOM,
-        SPECIAL_ENTITY
-    }
-
     private String name;
-    private String description;
     private String systemCode;
-    private Type type;
 
-
-    public DiagnosisCategory(String name, String description, String systemCode, Type type) {
+    public DiagnosisCategory(String name, String systemCode) {
         this.name = name;
-        this.description = description;
         this.systemCode = systemCode;
-        this.type = type;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
     public String getSystemCode() {
         return systemCode;
-    }
-
-    public Type getType() {
-        return type;
     }
 }
