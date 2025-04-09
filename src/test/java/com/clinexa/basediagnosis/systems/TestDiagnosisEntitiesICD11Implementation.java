3144cae23fc2d30926cf3ccd12c66254ed5a2414
Nikita Serba <nikitaserba@icloud.com>
2025-04-09 18:44:08 +0300

    Testing serialization (issue #3)

---
+++ src/test/java/com/clinexa/basediagnosis/systems/TestDiagnosisEntitiesICD11Implementation.java
@@ -0,0 +1,68 @@
+package com.clinexa.basediagnosis.systems;
+
+import com.clinexa.basediagnosis.DiagnosesSystem;
+import com.clinexa.basediagnosis.Diagnosis;
+import com.clinexa.basediagnosis.Symptom;
+import com.clinexa.basediagnosis.systems.misc.DataForToken;
+import com.clinexa.basediagnosis.utils.ICDLanguage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.opentest4j.TestAbortedException;
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestDiagnosisEntitiesICD11Implementation {
+
+    private DiagnosesSystem system;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        system = ICD11DiagnosesSystem.getInstance();
+        DataForToken dataForToken;
+        try {
+            dataForToken = TestTokenManager.getDataForToken();
+        } catch (RuntimeException e) {
+            throw new TestAbortedException("Skipped due to no token been found.");
+        }
+        system.setParameter(ICD11DiagnosesSystem.CLIENT_ID_KEY, dataForToken.getClientID());
+        system.setParameter(ICD11DiagnosesSystem.CLIENT_SECRET_KEY, dataForToken.getClientSecret());
+        system.init();
+    }
+
+    @Test
+    void testDiagnosisSerialization() throws IOException, ClassNotFoundException {
+        Diagnosis diagnosis = (Diagnosis) system.getByICD11Code("1A40.0");
+        var byteArrayOut = new ByteArrayOutputStream();
+        try (var out = new ObjectOutputStream(byteArrayOut)) {
+            out.writeObject(diagnosis);
+        }
+
+        var in = new ObjectInputStream(new ByteArrayInputStream(byteArrayOut.toByteArray()));
+        Diagnosis diagnosisFromStream = (Diagnosis) in.readObject();
+
+        assertEquals(diagnosis.getICD11Code(), diagnosisFromStream.getICD11Code());
+        assertEquals(diagnosis, diagnosisFromStream);
+        assertEquals(diagnosis.hashCode(), diagnosisFromStream.hashCode());
+        assertEquals(diagnosis.getTitle(ICDLanguage.RUSSIAN), diagnosisFromStream.getTitle(ICDLanguage.RUSSIAN));
+    }
+
+    @Test
+    void testSymptomSerialization() throws IOException, ClassNotFoundException {
+        Symptom diagnosis = (Symptom) system.getByICD11Code("MG43.0");
+        var byteArrayOut = new ByteArrayOutputStream();
+        try (var out = new ObjectOutputStream(byteArrayOut)) {
+            out.writeObject(diagnosis);
+        }
+
+        var in = new ObjectInputStream(new ByteArrayInputStream(byteArrayOut.toByteArray()));
+        Symptom diagnosisFromStream = (Symptom) in.readObject();
+
+        assertEquals(diagnosis.getICD11Code(), diagnosisFromStream.getICD11Code());
+        assertEquals(diagnosis, diagnosisFromStream);
+        assertEquals(diagnosis.hashCode(), diagnosisFromStream.hashCode());
+        assertEquals(diagnosis.getTitle(ICDLanguage.RUSSIAN), diagnosisFromStream.getTitle(ICDLanguage.RUSSIAN));
+    }
+
+}
